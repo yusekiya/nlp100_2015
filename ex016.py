5b49e47290a8ab04c0fabb3735e3b4d04cd86e47
yusekiya <y.seki.14870@gmail.com>
2016-05-09 04:39:49 +0900

    Bug fix

--- ex016.py
+++ ex016.py
@@ -12,8 +12,9 @@ with open('hightemp.txt', 'r') as f:
         print('Chunk{}:'.format(ind))
         bs = q + 1
         print(''.join(lines[bs*i:bs*(i+1)]))
-    for i in range(r, N):
+    for i in range(N-r):
         ind += 1
         print('Chunk{}:'.format(ind))
         bs = q
+        lines = lines[(q+1)*r:]
         print(''.join(lines[bs*i:bs*(i+1)]))
