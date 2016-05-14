0fe1626d07a720b0c3835d5814f006f620ab6f2d
yusekiya <y.seki.14870@gmail.com>
2016-05-14 18:56:06 +0900

    Solve exercise 034

---
+++ Ch4/ex034.py
@@ -0,0 +1,25 @@
+# 2つの名詞が「の」で連結されている名詞句を抽出せよ．
+# 「の」は助詞,連体化
+import ex030
+
+def extract_noun_phrase():
+    result = ex030.morpheme_analysis()
+    ret = set()
+    for sentence in result:
+        pos1_list = [mopheme['pos1'] for mopheme in sentence]
+        adnomial_ind = (index for index, string in enumerate(pos1_list) if string == '連体化')
+        for i in adnomial_ind:
+            try:
+                noun_phrase = sentence[i-1]['surface'] + 'の' + sentence[i+1]['surface']
+                ret.add(noun_phrase)
+            except IndexError:
+                pass
+    return ret
+
+
+def main():
+    print(extract_noun_phrase())
+
+
+if __name__ == '__main__':
+    main()
