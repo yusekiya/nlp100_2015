529c13a99e74832d662d64f36a08fd766940c7d0
yusekiya <y.seki.14870@gmail.com>
2016-05-23 21:18:46 +0900

    Solve exercise 044

---
+++ Ch5/draw_dot.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/usr/local/bin/dot -Tjpg ${1:-output.dot} -o ${2:-output.jpg}
---
+++ Ch5/ex044.py
@@ -0,0 +1,44 @@
+# 与えられた文の係り受け木を有向グラフとして可視化せよ．
+# 可視化には，係り受け木をDOT言語に変換し，Graphvizを用いるとよい．
+# また，Pythonから有向グラフを直接的に可視化するには，pydotを使うとよい．
+import ex041
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def main():
+    chunks = ex041.generate_chunks_whole_sentence()
+    res = []
+    for sentence in chunks:
+        surface_dst = [(join_surfaces_in_chunk(chunk), chunk.dst) for chunk in sentence]
+        dependency = [('{}'.format(surface), '{}'.format(surface_dst[dst][0]))\
+                      for surface, dst in surface_dst if surface and dst!=-1]
+        if dependency:
+            res.append(dependency)
+    # output dot lang for n-th sentence
+    output_dot(res[8])
+    return 0
+
+
+def join_surfaces_in_chunk(chunk):
+    surfaces = [m.surface for m in chunk.morphs if m.pos != '記号']
+    return ''.join(surfaces)
+
+
+def plot_network(DiGraph):
+    pos = nx.spring_layout(DiGraph)
+    nx.draw_networkx_nodes(DiGraph, pos, node_size=5000, node_color='white')
+    nx.draw_networkx_edges(DiGraph, pos)
+    nx.draw_networkx_labels(DiGraph, pos, font_family='Osaka', font_size=20)
+    plt.show()
+
+
+def output_dot(dependency, fname='./output.dot'):
+    DG = nx.DiGraph()
+    DG.add_edges_from(dependency)
+    nx.set_node_attributes(DG, 'fontname', 'Osaka')
+    nx.nx_agraph.write_dot(DG, fname)
+
+
+if __name__ == '__main__':
+    main()
+    print('finished')
